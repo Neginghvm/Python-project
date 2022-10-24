0400f26c04718a257f4be1ca6c91db3240a048e1
Neginghvm <116072122+Neginghvm@users.noreply.github.com>
2022-10-24 11:06:02 +0330

    Create EX1_4.py

---
+++ EX1/EX1_4.py
@@ -0,0 +1,21 @@
+def EX1_4(day):
+  '''
+  the program should display the corresponding day of the week and returns an error message ( this is not correct)
+  if the user enter a number that is outside the range of 1 throught 7.
+'''
+  if day==1:
+    terurn "MONDAY"
+    elif day==2:
+      return "TUESDAY"
+    elif day==3:
+      return "WEDNESDAY"
+    elif day==4:
+      return "THURSDAY" 
+    elif day==5:
+      return "FRIDAY"
+    elif day==6:
+      return "SATURDAY"
+    elif day==7:
+      return "SUNDAY"
+    else:
+      return "this is not correct"
