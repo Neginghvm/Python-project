737e3e847b51e5fcde351f1badb67f32d0341faa
Neginghvm <116072122+Neginghvm@users.noreply.github.com>
2022-10-24 20:25:26 +0330

    Update EX1_3.py

--- EX1/EX1_3.py
+++ EX1/EX1_3.py
@@ -1,6 +1,9 @@
 def EX1_3(side1, side2):
   '''
   (side1+side2)=maximum range of third edge and the length of the triangle are positive int
+  example:
+  >>>EX1_3(8, 10)
+  17
   '''
   
   if side1>0 and side2>0:
