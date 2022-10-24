cde1c5121a319ff20ed3edcb5c42b82e70e359e2
Neginghvm <116072122+Neginghvm@users.noreply.github.com>
2022-10-24 10:44:16 +0330

    Create EX1_2.py

---
+++ EX1/EX1_2.py
@@ -0,0 +1,10 @@
+def EX1_2(x, y):
+  '''
+  the area of a triangle is (base*height)/2 
+  '''
+  if x>0 and y>0:
+    aot= (x*y)/2
+    return aot
+  else:
+    return "this triangle is not correct"
+  
