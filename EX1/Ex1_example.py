8ac42cdb149dfd30b6abac66f4fe490275719535
Neginghvm <116072122+Neginghvm@users.noreply.github.com>
2022-10-24 20:30:44 +0330

    Update Ex1_example.py

--- EX1/Ex1_example.py
+++ EX1/Ex1_example.py
@@ -1,4 +1,12 @@
 def EX1_example(num_minutes):
+  '''
+  (int) -> int
+  return the number of seconds there are in num_minutes minutes variable.
+  example:
+  >>> EX1_example(6)
+  360
+  '''
+  
   if type(num_minutes)==int:
     seconds=num_minutes*60
     return(seconds)
